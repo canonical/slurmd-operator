828371729c29aa08a46280909e2903e982d27bef
David Gomez <david.gomez@canonical.com>
2023-01-20 12:55:55 -0800

    mpi integration test + mpi bug fix

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -3,7 +3,9 @@
 # See LICENSE file for licensing details.
 
 import asyncio
-import pytest 
+import pytest
+import requests
+import time
 import yaml
 
 from helpers import (
@@ -11,8 +13,13 @@ from helpers import (
     fetch_slurmctld_deps,
 )
 
+from subprocess import PIPE, check_output
+
 from pathlib import Path
 from pytest_operator.plugin import OpsTest
+from tenacity import retry
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_exponential as wexp
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 NHC = METADATA["resources"]["nhc"]["filename"]
@@ -89,3 +96,31 @@ async def test_build_and_deploy_success(ops_test: OpsTest, series: str, slurmd_c
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(apps=[SLURMD], status="active", timeout=1000)
         assert ops_test.model.applications[SLURMD].units[0].workload_status == "active"
+
+@pytest.mark.abort_on_fail
+async def test_mpi_install(ops_test: OpsTest):
+    unit = ops_test.model.applications[SLURMD].units[0]
+    action = await unit.run_action("mpi-install")
+    result = await action.wait()
+    result = check_output(
+        f"JUJU_MODEL={ops_test.model_full_name} juju exec --unit slurmd/0 mpirun --version",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+    time.sleep(5)  # Wait 
+    assert "buckets" in result
+
+"""
+@pytest.mark.abort_on_fail
+@retry(wait=wexp(multiplier=2, min=1, max=30), stop=stop_after_attempt(10), reraise=True)
+async def test_application_is_up(ops_test: OpsTest):
+    status = await ops_test.model.get_status() 
+    unit = list(status.applications[SLURMCTLD].units)[0]
+    address = status["applications"][SLURMCTLD]["units"][unit]["public-address"]
+    # Test ETCD3
+    response = requests.get(f"http://{address}:2379/v3/")
+    assert response.status_code == 200
+    # response = requests.get(f"http://{address}:6818/metrics")
+    # assert response.status_code == 200
+"""
